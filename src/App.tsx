3f42d1730f9c2d02c3e2c343164a1979760be9ec
guoruifeng <guoruifeng@bjuci.com.cn>
2023-05-11 15:53:54 +0800

    feat: 编写玩layout相关内容

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import MyLayout from './components/organisms/MyLayout'
 import './app.less'
+import { Button } from "antd";
 
 const App: React.FC = () => {
   return (
-    <MyLayout></MyLayout>
+    <MyLayout>
+      <Button type="primary">123</Button>
+    </MyLayout>
   )
 }
 
--- src/components/molecules/MyLayoutContent.tsx
+++ src/components/molecules/MyLayoutContent.tsx
@@ -1,19 +1,20 @@
-import React, { type FC, memo } from 'react'
+import React, { type FC, type ReactNode, memo } from 'react'
 import { Breadcrumb, Layout } from 'antd'
 
 export type myLayoutContent = {
   color: string
+  children: ReactNode
 }
 
-const MyLayoutContent: FC<myLayoutContent> = memo<myLayoutContent>(({ color }) => {
+const MyLayoutContent: FC<myLayoutContent> = memo<myLayoutContent>(({ color, children }) => {
   return (
     <Layout.Content style={{ margin: '0 16px' }}>
       <Breadcrumb style={{ margin: '16px 0' }}>
         <Breadcrumb.Item>User</Breadcrumb.Item>
         <Breadcrumb.Item>Bill</Breadcrumb.Item>
       </Breadcrumb>
-      <div style={{ padding: 24, height: '100%', background: color }}>
-        Bill is a cat.
+      <div style={{ padding: 24, height: 'calc(100% - 54px)', background: color }}>
+        {children}
       </div>
     </Layout.Content>
   )
