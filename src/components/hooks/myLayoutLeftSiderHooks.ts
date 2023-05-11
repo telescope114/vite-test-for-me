e5560f8d21f66ab2484094c92d317c9baf2fc482
guoruifeng <guoruifeng@bjuci.com.cn>
2023-05-11 16:56:42 +0800

    fix: layoutSider菜单封装

--- src/components/hooks/myLayoutLeftSiderHooks.ts
+++ src/components/hooks/myLayoutLeftSiderHooks.ts
@@ -1,25 +1,45 @@
 import { MenuProps } from 'antd'
 import { type ReactNode } from 'react'
 
-export type MenuItem = Required<MenuProps>['items'][number]
+export type menuItem = Required<MenuProps>['items'][number]
 
 /**
  * 每个菜单规范格式
  * @param label: {ReactNode}
  * @param key: {string | number}
  * @param icon {ReactNode}
- * @param children {MenuItem[]}
+ * @param children {menuItem[]}
  */
 export const getItem = (
   label: ReactNode,
   key: string | number,
   icon?: ReactNode,
-  children?: MenuItem[],
-): MenuItem => {
+  children?: menuItem[],
+): menuItem => {
   return {
     key,
     icon,
     children,
     label,
-  } as MenuItem
+  } as menuItem
+}
+
+export type menuDataItem = {
+  label: ReactNode
+  key: string | number
+  icon?: ReactNode
+  children?: menuDataItem[]
+}
+
+export const getMenuData = (MenuDataItem: menuDataItem[]): menuItem[] => {
+  const out: menuItem[] = []
+  for (const item of MenuDataItem) {
+    if (item.children) {
+      let children: menuItem[] = getMenuData(item.children)
+      out.push(getItem(item.label, item.key, item.icon, children))
+    } else {
+      out.push(getItem(item.label, item.key, item.icon))
+    }
+  }
+  return out
 }
--- src/components/molecules/MyLayoutLeftSider.tsx
+++ src/components/molecules/MyLayoutLeftSider.tsx
@@ -1,26 +1,29 @@
 import React, {type FC, memo, useState} from 'react'
 import { Menu, Layout } from 'antd'
 import { DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons'
-import { type MenuItem ,getItem } from '../hooks/myLayoutLeftSiderHooks'
+import { type menuItem, getMenuData } from '../hooks/myLayoutLeftSiderHooks'
 
 const MyLayoutLeftSider: FC = memo(() => {
   const [collapsed, setCollapsed] = useState(false)
-  const items: MenuItem[] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
-      getItem('Tom', '3'),
-      getItem('Bill', '4'),
-      getItem('Alex', '5')
-    ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />)
-  ]
+  const menuData: menuItem[] = getMenuData([
+    { label: 'Option 1', key: '1', icon: (<PieChartOutlined />) },
+    { label: 'Option 2', key: '2', icon: <DesktopOutlined/>},
+    { label: 'User', key: 'sub1', icon: (<UserOutlined/>), children: [
+      { label: 'Tom', key: '3'},
+      { label: 'Bill', key: '4'},
+      { label: 'Alex', key: '5'}
+    ]},
+    { label: 'Team', key: 'sub2', icon: (<TeamOutlined/>), children: [
+        { label: 'Team 1', key: '6'},
+        { label: 'Team 2', key: '8'}
+    ]},
+    { label: 'Files', key: '9', icon: (<FileOutlined/>)}
+  ])
 
   return (
     <Layout.Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
       <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
-      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={menuData} />
     </Layout.Sider>
   )
 })
