9760261e852035d32d48020ab002cb847a65e699
guoruifeng <guoruifeng@bjuci.com.cn>
2023-05-12 10:43:57 +0800

    fix: 优化router传参

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,24 @@
-import {createBrowserRouter, Navigate} from 'react-router-dom'
-import React from 'react'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
+import React, { type ReactNode, type ElementType } from 'react'
 import Login from '../pages/login'
-import App from '../App'
+import MyLayout from '../components/organisms/MyLayout'
 import NotFound from '../pages/notFound'
+import Aaa from '../pages/aaa'
+
+const nestComponents = (components: ElementType[]): ReactNode => {
+  if (components.length <= 0) {
+    return null
+  }
+  const [FirstComponent, ...rest] = components
+  return (
+    <FirstComponent>{ nestComponents(rest) }</FirstComponent>
+  )
+}
 
 const router = createBrowserRouter([
   { path: '/login', element: (<Login />) },
-  { path: '/app', element: (<App/>)},
+  { path: '/app/aaa', element: (nestComponents([MyLayout, Aaa])) },
+  { path: '/app', element: (<MyLayout/>) },
   { path: '/', element: (<Navigate to="/app"/>) },
   { path: '*', element: (<NotFound />) }
 ])
