d8538abbd9196ceda9a56b7b99ff68d1c0ead499
guoruifeng <guoruifeng@bjuci.com.cn>
2023-05-16 09:51:25 +0800

    feat: 存储锚点1

--- src/router/withoutAuth.tsx
+++ src/router/withoutAuth.tsx
@@ -5,8 +5,10 @@ import React from 'react'
 /**
  * 无需身份认证的路由
  */
-const routes: RouteObject[] = [
-  { path: '/login', element: (<Login />) }
-]
+const routes = (): RouteObject[] => {
+  return [
+    { path: '/login', element: (<Login />) }
+  ]
+}
 
 export default routes
